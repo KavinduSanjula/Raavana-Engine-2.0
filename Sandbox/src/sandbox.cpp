d4f8ecd9e83285e2bcc8a43f5db85c1a8b6d93bc
Kavindu Sanjula Dasanayake <kavindusanjula.1@gmail.com>
2022-09-05 11:20:12 +0530

    Test changes

--- Sandbox/src/sandbox.cpp
+++ Sandbox/src/sandbox.cpp
@@ -9,15 +9,31 @@ public:
 
 	}
 
+	void Start() override {
+		std::cout << "Starting Application" << std::endl;
+	}
+
 	void Loop() override {
 		//std::cout << "Loop runnig..." << std::endl;
 	}
 
 
-	void OnKeyPressed(KeyPressed* e) const {
+	void OnKeyPressed(KeyPressed* e) const override{
 		if (e->GetKey() == RE_Key::A) {
 			std::cout << "A key pressed" << std::endl;
 		}
+	}
+
+	void OnMouseButtonPressed(MouseButtonPressed* e) const override {
+
+		auto [x, y] = e->GetMousePosition();
+		std::cout << "(" << e->GetButton() << " - " << x << ", " << y << ")" << std::endl;
+
+	}
+
+	void OnMouseMoved(MouseMoved* e) const override {
+		auto [x, y] = e->GetMousePosition();
+		std::cout << "(" << x << ", " << y << ")" << std::endl;
 	}
 
 };
